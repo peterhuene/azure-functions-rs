29e8a534d3cb2f13e4329e8ae33c0d8e55712ee8
Peter Huene <pehuene@microsoft.com>
2018-11-05 13:35:13 -0800

    Fix the use of cached protobuf output for building crate documentation.
    This commit fixes the use of the cached protobuf output for environments where
    `protoc` is not on PATH.
    
    This should allow docs.rs to build documentation for our crates.
    
    Fixes #41.

--- azure-functions-shared/build.rs
+++ azure-functions-shared/build.rs
@@ -1,71 +1,61 @@
+extern crate protoc;
 extern crate protoc_grpcio;
-extern crate reqwest;
 
+use protoc::Protoc;
 use std::env;
 use std::fs;
-use std::fs::File;
 use std::path::PathBuf;
 
-// TODO: revisit using this as the source for the cached files
-const BASE_URL: &'static str =
-    "https://raw.githubusercontent.com/peterhuene/azure-functions-rs/master/cache/";
 const OUT_DIR_VAR: &'static str = "OUT_DIR";
 const CACHE_DIR_NAME: &'static str = "cache";
 const PROTOBUF_INPUT_FILE: &'static str = "FunctionRpc.proto";
 const RUST_PROTOBUF_FILE: &'static str = "FunctionRpc.rs";
 const RUST_GRPC_FILE: &'static str = "FunctionRpc_grpc.rs";
 
-fn download_file(name: &str) {
-    let out_dir = PathBuf::from(env::var(OUT_DIR_VAR).unwrap());
-
-    let mut output = File::create(out_dir.join(name))
-        .expect(&format!("failed to create '{}' in output directory", name));
-
-    reqwest::get(&(BASE_URL.to_string() + name))
-        .expect(&format!("failed to download '{}' from the cache", name))
-        .error_for_status()
-        .expect("server responded with an error")
-        .copy_to(&mut output)
-        .expect(&format!(
-            "failed to copy response to output file '{}'",
-            name
-        ));
-}
-
-fn download_cached_files() {
-    download_file(RUST_PROTOBUF_FILE);
-    download_file(RUST_GRPC_FILE);
-}
-
-fn compile_protobufs() {
-    let out_dir = PathBuf::from(env::var(OUT_DIR_VAR).unwrap());
-
+fn compile_protobufs(out_dir: &PathBuf, cache_dir: &PathBuf) {
     protoc_grpcio::compile_grpc_protos(&[PROTOBUF_INPUT_FILE], &["protobuf/src/proto"], &out_dir)
         .expect("Failed to compile gRPC definitions.");
-
-    let cache_dir = env::current_dir()
-        .expect("couldn't determine current working directory")
-        .parent()
-        .expect("expect working directory is not the root")
-        .join(CACHE_DIR_NAME);
-
-    fs::create_dir_all(&cache_dir).expect("failed to create cache directory");
 
     fs::copy(
         out_dir.join(RUST_PROTOBUF_FILE),
         cache_dir.join(RUST_PROTOBUF_FILE),
     )
     .expect(&format!("can't update cache file '{}'", RUST_PROTOBUF_FILE));
+
     fs::copy(out_dir.join(RUST_GRPC_FILE), cache_dir.join(RUST_GRPC_FILE))
         .expect(&format!("can't update cache file '{}'", RUST_GRPC_FILE));
+}
+
+fn use_cached_files(out_dir: &PathBuf, cache_dir: &PathBuf) {
+    fs::copy(
+        cache_dir.join(RUST_PROTOBUF_FILE),
+        out_dir.join(RUST_PROTOBUF_FILE),
+    )
+    .expect(&format!(
+        "can't copy cache file '{}' to output directory",
+        RUST_PROTOBUF_FILE
+    ));
+
+    fs::copy(cache_dir.join(RUST_GRPC_FILE), out_dir.join(RUST_GRPC_FILE)).expect(&format!(
+        "can't copy cache file '{}' to output directory",
+        RUST_GRPC_FILE
+    ));
 }
 
 fn main() {
     println!("cargo:rerun-if-changed=protobuf/src/proto/FunctionRpc.proto");
 
-    if cfg!(feature = "cached_protobufs") {
-        download_cached_files();
+    let out_dir = PathBuf::from(env::var(OUT_DIR_VAR).unwrap());
+
+    let cache_dir = env::current_dir()
+        .expect("couldn't determine current working directory")
+        .join(CACHE_DIR_NAME);
+
+    fs::create_dir_all(&cache_dir).expect("failed to create cache directory");
+
+    if Protoc::from_env_path().check().is_ok() {
+        compile_protobufs(&out_dir, &cache_dir);
     } else {
-        compile_protobufs();
+        use_cached_files(&out_dir, &cache_dir);
     }
 }
--- cache/README.md
+++
@@ -1,8 +0,0 @@
-# Protobuf Cache
-
-This directory exists to cache the output of a previous protobuf compilation.
-
-See the `cached_protobufs` feature of the crate of the `azure-functions-shared` crate.
-
-The feature is primarily used for crate publishing so that [docs.rs](https://docs.rs) can
-generate documentation without having a protobuf compiler installed.
