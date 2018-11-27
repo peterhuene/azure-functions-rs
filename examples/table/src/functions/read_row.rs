5140b748f9b5bf3512ca56e0f96075159198c4a2
Peter Huene <pehuene@microsoft.com>
2018-11-26 17:11:29 -0800

    Update the `table` example to use the SDK.
    This commit updates the `table` example to use the Azure Functions for Rust SDK.

---
+++ examples/table/src/functions/read_row.rs
@@ -0,0 +1,23 @@
+use azure_functions::bindings::{Blob, HttpRequest, HttpResponse, Table};
+use azure_functions::func;
+use serde_json::Value;
+
+#[func]
+#[binding(
+    name = "_req",
+    auth_level = "anonymous",
+    route = "read/{table}/{partition}/{row}"
+)]
+#[binding(
+    name = "table",
+    table_name = "{table}",
+    partition_key = "{partition}",
+    row_key = "{row}"
+)]
+#[binding(
+    name="output1", // TODO: remove this workaround binding
+    path="unused"
+)]
+pub fn read_row(_req: &HttpRequest, table: &Table) -> (HttpResponse, Option<Blob>) {
+    (table.as_value().get(0).unwrap_or(&Value::Null).into(), None)
+}
