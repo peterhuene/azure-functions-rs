fecd69a992dbe45cad2c1872d0ef48ed205410f6
theckert <theckert@microsoft.com>
2019-07-23 15:18:24 -0700

    Add tests for ActivityOutput

--- azure-functions/src/durable/activity_output.rs
+++ azure-functions/src/durable/activity_output.rs
@@ -1,7 +1,8 @@
+use crate::rpc::{typed_data::Data, TypedData};
 use serde_json::Value;
 
 /// # Activity Output
-/// 
+///
 /// Type returned by Activity Functions for Durable Functions
 
 struct ActivityOutput(serde_json::Value);
@@ -21,5 +22,35 @@ impl Into<TypedData> for ActivityOutput {
         TypedData {
             data: Some(Data::Json(self.0.to_string())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn it_converts_from_json() {
+        let activity_output: ActivityOutput = json!({ "foo": "bar" }).into();
+
+        let data: TypedData = activity_output.into();
+        assert_eq!(data.data, Some(Data::Json(r#"{"foo":"bar"}"#.to_string())));
+    }
+
+    #[test]
+    fn it_converts_from_bool() {
+        let activity_output: ActivityOutput = true.into();
+
+        let data: TypedData = activity_output.into();
+        assert_eq!(data.data, Some(Data::Json(true.to_string())));
+    }
+
+    #[test]
+    fn it_converts_from_string() {
+        let activity_output: ActivityOutput = "foo".into();
+
+        let data: TypedData = activity_output.into();
+        assert_eq!(data.data, Some(Data::Json("\"foo\"".to_string())));
     }
 }
