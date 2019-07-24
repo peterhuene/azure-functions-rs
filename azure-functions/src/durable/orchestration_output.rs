5493f1827a0fc39793be6cf4773d0af81eb0e5e0
t-eckert <t_eckert@outlook.com>
2019-07-24 10:14:17 -0700

    rm `Into<TypedData>` add `to_value()` add tests

--- azure-functions/src/durable/orchestration_output.rs
+++ azure-functions/src/durable/orchestration_output.rs
@@ -1,14 +1,13 @@
-use crate::rpc::{typed_data::Data, TypedData};
 use serde_json::Value;
 
 /// # Orchestration Output
-/// 
+///
 /// Type returned by Orchetrator Functions for Durable Functions
 
 struct OrchestrationOutput(serde_json::Value);
 
 impl<T> From<T> for OrchestrationOutput
-where 
+where
     T: Into<Value>,
 {
     fn from(t: T) -> Self {
@@ -16,12 +15,9 @@ where
     }
 }
 
-#[doc(hidden)]
-impl Into<TypedData> for OrchestrationOutput {
-    fn into(self) -> TypedData {
-        TypedData {
-            data: Some(Data::Json(self.0.to_string())),
-        }
+impl OrchestrationOutput {
+    pub(crate) fn to_value(self) -> Value {
+        self.0
     }
 }
 
@@ -34,23 +30,23 @@ mod tests {
     fn it_converts_from_json() {
         let orchestration_output: OrchestrationOutput = json!({"foo": "bar"}).into();
 
-        let data: TypedData = activity_output.into();
-        assert_eq!(data.data, Some(Data::Json(r#"{"foo":"bar"}"#.to_string())));
+        let data: Value = orchestration_output.to_value();
+        assert_eq!(data, json!({"foo": "bar"}));
     }
 
     #[test]
     fn it_converts_from_bool() {
         let orchestration_output: OrchestrationOutput = true.into();
 
-        let data: TypedData = Orchestration_output.into();
-        assert_eq!(data.data, Some(Data::Json(true.to_string())));
+        let data: Value = orchestration_output.to_value();
+        assert_eq!(data, json!(true));
     }
 
     #[test]
     fn it_converts_from_string() {
         let orchestration_output: OrchestrationOutput = "foo".into();
 
-        let data: TypedData = orchestration_output.into();
-        assert_eq!(data.data, Some(Data::Json("\"foo\"".to_string())));
+        let data: Value = orchestration_output.to_value();
+        assert_eq!(data, json!("\"foo\"".to_string()));
     }
 }
