a6f08a2bfd1968113c5fb4a7420c180d18c605c8
Denis Molokanov <dmolokanov@users.noreply.github.com>
2019-07-23 18:22:17 -0700

    Add call_activity method stub (#366)
    Add call_activity method implemented without current_time updates and marking history events as processed

--- azure-functions/src/bindings/durable_orchestration_context.rs
+++ azure-functions/src/bindings/durable_orchestration_context.rs
@@ -2,11 +2,14 @@ use serde::Deserialize;
 use serde_json::{from_str, Value};
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use crate::durable::HistoryEvent;
+use crate::durable::{Action, EventType, HistoryEvent};
 use crate::{
     durable::ExecutionResult,
     rpc::{typed_data::Data, TypedData},
 };
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 /// Represents the Durable Functions orchestration context binding.
 ///
@@ -33,7 +36,7 @@ struct DurableOrchestrationContextData {
     is_replaying: bool,
     parent_instance_id: Option<String>,
     input: Value,
-    history: Option<Vec<HistoryEvent>>,
+    history: Vec<HistoryEvent>,
 }
 
 impl DurableOrchestrationContext {
@@ -71,6 +74,87 @@ impl DurableOrchestrationContext {
     #[doc(hidden)]
     pub fn execution_result(&self) -> Rc<RefCell<ExecutionResult>> {
         self.result.clone()
+    }
+
+    #[doc(hidden)]
+    pub fn call_activity<D>(
+        &mut self,
+        activity_name: &str,
+        data: D,
+    ) -> impl Future<Output = Result<Value, String>>
+    where
+        D: Into<Value>,
+    {
+        // TODO Assume each of these methods mark found entries as processed
+        // TODO They will also be responsible for moving the "current" timestamp forward as they process the execution history
+        let scheduled = self.find_scheduled_activity(activity_name);
+        let completed = scheduled.and_then(|scheduled| self.find_completed_activity(scheduled));
+        let failed = scheduled.and_then(|scheduled| self.find_failed_activity(scheduled));
+
+        self.result.borrow_mut().add_action(Action::CallActivity {
+            function_name: activity_name.to_string(),
+            input: data.into(),
+        });
+
+        if let Some(completed) = completed {
+            return ActionFuture(Some(Ok(self.parse_history_event(completed))));
+        }
+
+        if let Some(failed) = failed {
+            return ActionFuture(Some(Err(failed.reason.clone().unwrap_or_default())));
+        }
+
+        return ActionFuture(None);
+    }
+
+    fn find_scheduled_activity(&self, activity_name: &str) -> Option<&HistoryEvent> {
+        self.data.history.iter().find(|event| {
+            event.name == Some(activity_name.to_owned())
+                && event.event_type == EventType::TaskScheduled
+                && !event.is_processed
+        })
+    }
+
+    fn find_completed_activity(&self, scheduled: &HistoryEvent) -> Option<&HistoryEvent> {
+        self.data.history.iter().find(|event| {
+            event.event_type == EventType::TaskCompleted
+                && event.task_scheduled_id == Some(scheduled.event_id)
+        })
+    }
+
+    fn find_failed_activity(&self, scheduled: &HistoryEvent) -> Option<&HistoryEvent> {
+        self.data.history.iter().find(|event| {
+            event.event_type == EventType::TaskFailed
+                && event.task_scheduled_id == Some(scheduled.event_id)
+        })
+    }
+
+    fn parse_history_event(&self, event: &HistoryEvent) -> Value {
+        match event.event_type {
+            EventType::EventRaised => event.input.clone(),
+            EventType::SubOrchestrationInstanceCompleted | EventType::TaskCompleted => {
+                event.result.clone()
+            }
+            _ => None,
+        }
+        .unwrap_or_default()
+    }
+}
+
+struct ActionFuture<T>(Option<T>);
+
+impl<T> Future for ActionFuture<T>
+where
+    T: Unpin,
+{
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, _context: &mut Context) -> Poll<T> {
+        if let Some(v) = self.get_mut().0.take() {
+            return Poll::Ready(v);
+        }
+
+        Poll::Pending
     }
 }
 
@@ -141,7 +225,8 @@ mod tests {
                 "instanceId":"49497890673e4a75ab380e7a956c607b",
                 "isReplaying":false,
                 "parentInstanceId":"1234123412341234123412341234",
-                "input": []
+                "input": [],
+                "history": []
             }"#
                 .to_owned(),
             )),
@@ -154,7 +239,7 @@ mod tests {
             Some("1234123412341234123412341234")
         );
         assert!(!context.is_replaying());
-        assert_eq!(context.data.history, None);
+        assert_eq!(context.data.history, vec![]);
         assert_eq!(context.input(), &serde_json::Value::Array(vec![]));
     }
 
@@ -165,29 +250,27 @@ mod tests {
                 r#"{
                 "history":[
                     {
-                        "EventType":12,
+                       "EventType":12,
+                       "EventId":-1,
+                       "IsPlayed":false,
+                       "Timestamp":"2019-07-18T06:22:27.016757Z"
+                    },
+                    {
+                        "OrchestrationInstance":{
+                           "InstanceId":"49497890673e4a75ab380e7a956c607b",
+                           "ExecutionId":"5d2025984bef476bbaacefaa499a4f5f"
+                        },
+                        "EventType":0,
+                        "ParentInstance":null,
+                        "Name":"HelloWorld",
+                        "Version":"",
+                        "Input":"{}",
+                        "Tags":null,
                         "EventId":-1,
                         "IsPlayed":false,
-                        "Timestamp":"2019-07-18T06:22:27.016757Z"
+                       "Timestamp":"2019-07-18T06:22:26.626966Z"
                     }
-
-                 ,
-                  {
-                     "OrchestrationInstance":{
-                        "InstanceId":"49497890673e4a75ab380e7a956c607b",
-                        "ExecutionId":"5d2025984bef476bbaacefaa499a4f5f"
-                     },
-                     "EventType":0,
-                     "ParentInstance":null,
-                     "Name":"HelloWorld",
-                     "Version":"",
-                     "Input":"{}",
-                     "Tags":null,
-                     "EventId":-1,
-                     "IsPlayed":false,
-                     "Timestamp":"2019-07-18T06:22:26.626966Z"
-                  }
-                  ],
+                ],
                 "instanceId":"49497890673e4a75ab380e7a956c607b",
                 "isReplaying":false,
                 "parentInstanceId":null,
@@ -204,7 +287,7 @@ mod tests {
         assert_eq!(context.input(), &serde_json::Value::Array(vec![]));
         assert_eq!(
             context.data.history,
-            Some(vec![
+            vec![
                 HistoryEvent {
                     event_type: EventType::OrchestratorStarted,
                     event_id: -1,
@@ -228,7 +311,7 @@ mod tests {
                     timestamp: DateTime::parse_from_rfc3339("2019-07-18T06:22:26.626966Z").unwrap(),
                     is_processed: false,
                     name: Some("HelloWorld".to_owned()),
-                    input: Some("{}".to_owned()),
+                    input: Some("{}".into()),
                     result: None,
                     task_scheduled_id: None,
                     instance_id: None,
@@ -237,7 +320,7 @@ mod tests {
                     fire_at: None,
                     timer_id: None
                 }
-            ])
+            ]
         );
     }
 }
