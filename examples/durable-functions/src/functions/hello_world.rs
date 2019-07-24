b8503c1df2edd6b6f83b44116e94032f6a62a6ec
Peter Huene <pehuene@microsoft.com>
2019-07-24 13:49:32 -0700

    Update Durable Functions example to use snake_case.
    This makes the example more "Rust".

--- examples/durable-functions/src/functions/hello_world.rs
+++ examples/durable-functions/src/functions/hello_world.rs
@@ -1,13 +1,13 @@
 use azure_functions::{bindings::DurableOrchestrationContext, durable::OrchestrationOutput, func};
 use futures::future::join_all;
 
-#[func(name = "HelloWorld")]
+#[func]
 pub async fn hello_world(mut context: DurableOrchestrationContext) -> OrchestrationOutput {
     // join_all(
     //     [
-    //         context.call_activity("SayHello", "Tokyo"),
-    //         context.call_activity("SayHello", "London"),
-    //         context.call_activity("SayHello", "Seattle"),
+    //         context.call_activity("say_hello", "Tokyo"),
+    //         context.call_activity("say_hello", "London"),
+    //         context.call_activity("say_hello", "Seattle"),
     //     ]
     //     .into_iter(),
     // )
